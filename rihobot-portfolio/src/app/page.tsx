546c4d388ff53c7d4aaf1eabb29580735f775ad1
Rehoboth <137082978+Robinadev@users.noreply.github.com>
2025-12-16 11:31:07 -0800

    ensured the bugging issue with blog , hero section

--- rihobot-portfolio/src/app/page.tsx
+++ rihobot-portfolio/src/app/page.tsx
@@ -4,14 +4,14 @@ import { Projects } from '@/components/projects'
 import { Skills } from '@/components/Skills';
 import { Contact } from '../../components/Contact';
 import { ThemeProvider } from '../../components/theme-provider';
-import { Blog } from '@/components/Blog';
+// import { Blog } from '@/components/Blog';
 export default function Home() {
   return (
     <>
       <Hero />
       <Experience />
       <Projects />
-      <Blog />
+        {/* <Blog /> */}
       <Skills />
       <Contact />
        <ThemeProvider />
